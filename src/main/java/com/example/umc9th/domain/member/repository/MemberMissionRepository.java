2e05c545b28ecdc2b50a179c5ac98b76ec139878
tnqlsqkr <psb0304022@gmail.com>
2025-10-28 11:53:34 +0900

    ✨ feat: Chapter 5 기능 구현

---
+++ src/main/java/com/example/umc9th/domain/member/repository/MemberMissionRepository.java
@@ -0,0 +1,20 @@
+package com.example.umc9th.domain.member.repository;
+
+import com.example.umc9th.domain.member.entity.mapping.MemberMission;
+import com.example.umc9th.domain.member.enums.MissionStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long> {
+
+    Page<MemberMission> findAllByMemberIdAndStatus(
+            Long memberId,
+            MissionStatus status,
+            Pageable pageable
+    );
+
+    List<MemberMission> findAllByMemberId(Long memberId);
+}
